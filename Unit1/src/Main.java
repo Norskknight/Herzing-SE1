6e03d016ffead6aaf4aeea45088575e252d474e7
John spink <johnspink11i@yahoo.com>
2022-09-11 16:07:35 -0500

    init comments on assignment

---
+++ Unit1/src/Main.java
@@ -0,0 +1,28 @@
+//The code must run (and a screenshot of the running code in the ReadMe file on GitHub)
+//If the terminal/console is not in the screenshot, it will not have worked as it will be considered wrong.
+//The game should be two players, so no AI (unless you want that blue ribbon)
+//There should be a while loop asking players to either continue playing or stop playing after each round (y/n answer)
+//Player input can either be a string or integer to select rack/paper/scissors.
+//A tally of wins should be kept, and after the player chooses to end the game, the tally should print out for both players.
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        //start
+        System.out.println("player 1 enter:");
+        System.out.println("1 : rock");
+        System.out.println("2 : paper");
+        System.out.println("3 : scissors");
+        //player 1 goes
+
+        //player 2 goes
+        //display result
+        //end
+        //add to score
+        //again y/n
+        //if n score print out
+
+    }
+}
